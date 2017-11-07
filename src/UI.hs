8e27fb8c7adc882d78ccd3719aab9a03869603c4
Enzo Haussecker <enzo@sovereign.io>
2017-11-07 11:25:10 -0800

    Clean-up API endpoints

--- src/UI.hs
+++ src/UI.hs
@@ -20,6 +20,7 @@ import Network.Wai.EventSource.EventStream (ServerEvent(..), eventToBuilder)
 import Web.Scotty.Trans                    (ScottyT, get, json, jsonData, post, scottyT, setHeader, stream)
 
 import Config
+import Log
 
 runUI
   :: MonadBaseControl IO m
@@ -38,32 +39,42 @@ app
   => MonadIO m
   => ScottyT Text m ()
 app = do
+
   get "/addresses" $ do
+    writeLog Debug "GET /addresses"
     Config {..} <- ask
     json $ cfgAddresses
-  get "/peers" $ do
-    Config {..} <- ask
-    count <- liftBase $ peers cfgInterface
-    json count
-  get "/sse" $ do
+
+  get "/messages" $ do
+    writeLog Debug "GET /messages"
     Config {..} <- ask
     messages <- liftBase $ dupChan cfgInbound
     setHeader "Content-Type" "text/event-stream"
-    stream $ source messages
+    stream $ sse messages
+
+  get "/peers" $ do
+    writeLog Debug "GET /peers"
+    Config {..} <- ask
+    count <- liftBase $ peers cfgInterface
+    json $ pred count
+
   get "/streams" $ do
+    writeLog Debug "GET /streams"
     Config {..} <- ask
     count <- liftBase $ streams cfgInterface
     json count
-  post "/chirp" $ do
+
+  post "/message" $ do
+    writeLog Debug "POST /message"
     Config {..} <- ask
     message <- jsonData
     liftBase $ atomically $ writeTQueue cfgOutbound message
 
-source
+sse
   :: ToJSON a
   => Chan a
   -> StreamingBody
-source chan send flush = fix $ \ loop -> do
+sse chan send flush = fix $ \ loop -> do
   builder <- fromLazyByteString . encode <$> readChan chan
   case eventToBuilder $ ServerEvent Nothing Nothing [builder] of
     Nothing -> pure ()
